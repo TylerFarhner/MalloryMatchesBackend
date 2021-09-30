f3054740bd82154184d240103b8ee695cdfe65c1
Tyler Farhner <tylerfarhner@gmail.com>
2021-09-30 16:15:58 -0700

    builds index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose = require('mongoose')
+// ------------------------------------------
+
+const matches = require('./routes/spots')
+
+const app = express()
+// ------------------------------------------
+// Middleware
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Mallory Matches API!')
+})
+
+app.use('/api/matches', matches)
+
+require('dotenv').config()
+
+const port = process.env.port || 3000
+
+
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(result => {
+    app.listen(port, () => console.log('Server is listening on port ${port}'))
+})
+.catch(err => console.log(err))
